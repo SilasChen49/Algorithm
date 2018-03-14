95f4046f56943d03045fcf49072d2270787ae6d0
SilasChen49 <SilasChen49@gmail.com>
2018-03-14 16:30:09 -0400

    requirements

---
+++ lab1/lab/main1.hpp
@@ -0,0 +1,14 @@
+//
+//  main1.hpp
+//  Algorithm
+//
+//  Created by Mo Chen on 2018-02-08.
+//  Copyright © 2018 Mo Chen. All rights reserved.
+//
+
+#ifndef main1_hpp
+#define main1_hpp
+
+#include <stdio.h>
+
+#endif /* main1_hpp */
